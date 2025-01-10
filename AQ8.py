25bb2ac5d8ecd1353621577315c7d7544758133b
MAYUR GORE <mayurgore2025@gmail.com>
2025-01-10 10:14:34 +0530

    Add files via upload

---
+++ AQ8.py
@@ -0,0 +1,18 @@
+z
+def main():
+    Values=list()
+
+    print(" Enter values  have to add in list")
+    Size=int(input())
+
+    print("Enter the values")
+    for i in range (0,Size):
+        No=int(input())
+        Values.append(No)
+
+        print(Values)
+
+
+
+if __name__=="__main__":
+    main()
